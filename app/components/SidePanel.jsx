6707fa9bc59eea9c3843cddee3bcc6b716834ec2
Rakesh Arora <mannyisha808@gmail.com>
2024-05-22 00:05:11 +0530

    theme-switcher

--- app/components/SidePanel.jsx
+++ app/components/SidePanel.jsx
@@ -1,28 +1,45 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { FcGoogle } from "react-icons/fc";
 import { signIn, signOut } from 'next-auth/react';
 import DeveloperOptions from './DeveloperOptions';
+import { useTheme } from 'next-themes';
+import { LuSunMedium } from "react-icons/lu";
+import { MdDarkMode } from "react-icons/md";
+
+
 
 const SidePanel = ({ session, setDeveloperOptions }) => {
     const [open, setOpen] = useState(false);
+    const [mounted, setMounted] = useState(false);
+    const { theme, setTheme } = useTheme();
+
+
+    useEffect(() => {
+        setMounted(true);
+    }, [])
 
     return (
-        <div className='h-[98%] max-sm:hidden flex flex-col w-[22%] bg-gradient-radial overflow-hidden from-gray-200 to-white self-center shadow-xl rounded-tr-2xl rounded-br-2xl drop-shadow-sm justify-between'>
-            <div className='w-full p-5 flex justify-between dark:text-black'>
-                <img className='h-[40px] mr-8 rounded-full' src={session?.user.image}></img>
-                {session ?
-                    <button onClick={() => signOut()} className='bg-gray-200 h-9 border flex gap-2 hover:border-indigo-500 transition-all hover:shadow-2xl p-4 py-1 rounded-full'>
-                        Sign Out
-                    </button> :
-                    <button onClick={() => signIn("google")} className='bg-gray-200 h-9 border flex gap-2 hover:border-indigo-500 transition-all hover:shadow-2xl p-4 py-1 rounded-full'>
-                        Sign In
-                        <FcGoogle className='self-center size-5' />
-                    </button>
-                }
+        <div className='h-[98%] dark:bg-gradient-to-br dark:from-[#2a2a2a] dark:to-gray-900 max-sm:hidden flex flex-col w-[22%] bg-gradient-radial overflow-hidden from-gray-200 to-white self-center shadow-xl rounded-tr-2xl rounded-br-2xl drop-shadow-sm justify-start'>
+            <div className='h-full w-full '>
+                <div className='w-full p-5 flex justify-between dark:text-black'>
+                    <img className='h-[40px] mr-8 rounded-full' src={session?.user.image}></img>
+                    {session ?
+                        <button onClick={() => signOut()} className='bg-gray-200 h-9 border flex gap-2 hover:border-indigo-500 transition-all hover:shadow-2xl p-4 py-1 rounded-full'>
+                            Sign Out
+                        </button> :
+                        <button onClick={() => signIn("google")} className='bg-gray-200 h-9 border flex gap-2 hover:border-indigo-500 transition-all hover:shadow-2xl p-4 py-1 rounded-full'>
+                            Sign In
+                            <FcGoogle className='self-center size-5' />
+                        </button>
+                    }
+                </div>
+                <div className='flex w-full p-6 justify-end'>
+                    {mounted && <button onClick={() => { if (theme === "dark") { setTheme("light"); } else { setTheme("dark"); } }}>{theme && theme === "light" ? <MdDarkMode size={20} color='black' /> : <LuSunMedium size={20} color='white' />}</button>}
+                </div>
             </div>
             <div className='mb-5 p-2'>
                 <DeveloperOptions open={open} setOpen={setOpen} setDeveloperOptions={setDeveloperOptions} />
-                <h6 onClick={() => { setOpen(!open) }} className={`text-sm select-none w-fit p-4 py-1 rounded-full text-gray-500 hover:text-black font-thin cursor-pointer border border-gray-400 transition-all hover:border-black ${open ? "bg-white text-black font-semibold w-full text-center" : "bg-transparent text-gray-500 mx-4"}`}>{open ? "Close" : "Developer Options"}</h6>
+                <h6 onClick={() => { setOpen(!open) }} className={`text-sm select-none w-fit p-4 py-1 rounded-full text-gray-500  dark:bg-gray-200 hover:text-black font-thin cursor-pointer border border-gray-400 transition-all hover:border-black ${open ? "bg-white text-black font-semibold w-full text-center" : "bg-transparent text-gray-500 mx-4"}`}>{open ? "Close" : "Developer Options"}</h6>
             </div>
         </div>
     )
--- app/layout.jsx
+++ app/layout.jsx
@@ -8,17 +8,18 @@ export const metadata = {
   description: "( All Model at one playground ) UI Developed by Mohit Arora 9667067062"
 }
 
+
 const RootLayout = ({ children }) => {
     return (
-    <html lang="en" className='bg-white h-full'>
+    <html lang="en" className={`bg-white h-full`}>
       <head>
         <meta name='description' content='Developed By Mohit Arora 9667067062'></meta>
         <meta name='og:image' content='images/logo1.jpeg'></meta>
         <link rel="icon" href='images/logo3.jpeg'></link>
         <script src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs" type="module"></script>
       </head>
-      <body className='bg-no-repeat h-full bg-gradient-to-t from-[#f3e7e9] to-[#e3eeff]'>
-        <ThemeProvider>
+      <body className='bg-no-repeat h-full bg-gradient-to-t dark:from-[#29323c] dark:to-[#485563] from-[#f3e7e9] to-[#e3eeff]'>
+        <ThemeProvider defaultTheme='light' attribute='class'>
           <Providers>
             {children}
           </Providers>
